50b2392e61f3d349d829df71cea95f762137c569
Andrew Lane <andrew.lane@powerschool.com>
2022-10-04 15:45:38 -0400

    Break a unit test to test the CI

--- dbt_python_model_testing_exploration/models/example/high_value_customers_with_no_orders.py
+++ dbt_python_model_testing_exploration/models/example/high_value_customers_with_no_orders.py
@@ -1,6 +1,6 @@
 def model(dbt, session):
     dbt.config(materialized="table")
     customer_orders_df = dbt.ref("customer_orders")
-    return customer_orders_df.filter("o_orderkey is null and c_acctbal >= 5000").select(
+    return customer_orders_df.filter("o_orderkey is null and c_acctbal > 5000").select(
         ["c_custkey", "c_name", "c_acctbal"]
     )
